ad275833ac1bb5986a7d3a50f160c896f5d8b9da
Thomas Weißgerber <weissger@posteo.de>
2019-11-25 13:56:38 +0100

    setup.py + readme

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Blinkerc
+
+Blinkerc is a small extension of the [blinker package](https://pypi.org/project/blinker/) found in following
+[repository](https://github.com/jek/blinker) to allow for out of the box class signals.
--- setup.py
+++ setup.py
@@ -1,59 +1,22 @@
-# from sphinx.setup_command import BuildDoc
-import re
+import setuptools
 
-from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-# cmdclass = {'build_sphinx': BuildDoc}
-
-NAMEFILE = "pypadre/_name.py"
-verstrline = open(NAMEFILE, "rt").read()
-VSRE = r"^__name__ = ['\"]([^'\"]*)['\"]"
-result = re.search(VSRE, verstrline, re.M)
-if result:
-    name = result.group(1)
-else:
-    raise RuntimeError("Unable to find name string in %s." % (NAMEFILE,))
-
-VERSIONFILE = "pypadre/_version.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-result = re.search(VSRE, verstrline, re.M)
-if result:
-    version = result.group(1)
-    release = version
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
-
-print('VERSION:{version}'.format(version=version))
-
-with open('requirements.txt', 'r') as f:
-    required = f.read()
-
-install_requires = required
-
-setup(
-    name=name,
-    version=version,
-    # TODO switch to scm version
-    # use_scm_version=True,
-    # setup_requires=['setuptools_scm'],
-    packages=['pypadre'] + find_packages(exclude=["tests", "tests.*", "*.tests.*", "*.tests"]),
-    package_dir={'pypadre': 'pypadre'},
-    include_package_data=True,
-    url='https://padre-lab.eu',
-    license='GPL',
-    author='Michael Granitzer',
-    author_email='michael.granitzer@uni-passau.de',
-    description='PaDRe aims to solve problems about reproducibility',
-    entry_points='''
-       [console_scripts]
-       pypadre=pypadre.cli.pypadre:pypadre
-   ''',
-    install_requires=install_requires,
-    command_options={
-        'build_sphinx': {
-            'project': ('setup.py', name),
-            'version': ('setup.py', version),
-            'release': ('setup.py', release)}},
-    # for click setup see http://click.pocoo.org/6/setuptools/
+setuptools.setup(
+    name="blinkerc",
+    version="0.0.1",
+    author="weissger",
+    author_email="thomas.weissgerber@uni-passau.de ",
+    description="Class signals based on blinker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Weissger/blinkerc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
